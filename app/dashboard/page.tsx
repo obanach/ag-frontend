a8ec374669a83b24bc77bdd2c9aabdac7a3dc65c
obanach <oliwerbanach@gmail.com>
2023-11-04 23:55:35 +0100

    wip navbar init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import {SiteHeader} from "@/components/site-header";
+
+function DashboardPage() {
+    return (
+        <div>k
+            <SiteHeader />
+        </div>
+    );
+}
+
+export default DashboardPage;
---
+++ config/site.tsx
@@ -0,0 +1,6 @@
+export const siteConfig = {
+    name: "AutoGrow",
+    description: "Integrated system for auto growing plans in your own tent.",
+}
+
+export type SiteConfig = typeof siteConfig
