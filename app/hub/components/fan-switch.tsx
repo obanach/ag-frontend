31e9eed2a552352ca61649883909415f1b3cfdbf
Oliwer Banach <oliwerbanach@gmail.com>
2023-11-05 23:09:38 +0100

    add hub page and its components

---
+++ app/hub/components/fan-switch.tsx
@@ -0,0 +1,39 @@
+import {CircleIcon} from "@radix-ui/react-icons"
+import { Card } from "@/components/ui/card"
+import { Switch } from "@/components/ui/switch";
+import * as React from "react";
+import {BatteryFullIcon, FanIcon, RefreshCwIcon} from "lucide-react";
+
+export function FanSwitch() {
+  return (
+    <Card>
+      <div className="flex items-center justify-between p-5">
+          <div className="flex items-center space-x-2">
+            <div className="hidden items-center justify-center w-20 h-20 rounded-md md:flex">
+              <FanIcon strokeWidth="0.75" className="w-16 h-16" />
+            </div>
+            <div className="flex flex-col justify-items-stretch">
+              <h3 className="text-2xl font-semibold leading-none tracking-tight mb-3">Fan</h3>
+              <div className="flex space-x-4 text-sm text-muted-foreground">
+                  <div className="flex items-center">
+                    <CircleIcon className="mr-1 h-3 w-3 fill-green-400 text-green-400" />
+                    Online
+                  </div>
+                  <div className="flex items-center">
+                    <BatteryFullIcon className="mr-1 h-3 w-3" />
+                    50%
+                  </div>
+                  <div className="flex items-center">
+                    <RefreshCwIcon className="mr-1 h-3 w-3" />
+                    now
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <Switch id="airplane-mode" />
+          </div>
+        </div>
+    </Card>
+  )
+}
--- app/hub/page.tsx
+++ app/hub/page.tsx
@@ -1,10 +1,40 @@
+import {PageHeader, PageHeaderDescription, PageHeaderHeading} from "@/components/page-header";
+import Link from "next/link";
+import {Separator} from "@radix-ui/react-menu";
+import {ArrowRightIcon} from "@radix-ui/react-icons";
+import {cn} from "@/lib/utils";
+import {buttonVariants} from "@/components/ui/button";
+import {siteConfig} from "@/config/site";
+import {FanSwitch} from "@/app/hub/components/fan-switch";
+import {LightSwitch} from "@/app/hub/components/light-switch";
+import {AirTemperature} from "@/app/hub/components/air-temperature";
+import {AirHumidity} from "@/app/hub/components/air-humidity";
 
-function DevicePage() {
+function HubPage() {
     return (
         <div>
-            Device page
+            <PageHeader className="pb-8">
+                <PageHeaderHeading>My first Hub</PageHeaderHeading>
+                <PageHeaderDescription>
+                  View, configure and manage your hub and its modules remotely.
+                </PageHeaderDescription>
+            </PageHeader>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 col-span-2">
+                <div className="col-span-1 row-span-2">
+                  <FanSwitch />
+                </div>
+                <div className="col-span-1 row-span-2">
+                  <LightSwitch />
+                </div>
+                <div className="col-span-1 row-span-2">
+                  <AirTemperature />
+                </div>
+                <div className="col-span-1 row-span-2">
+                  <AirHumidity />
+                </div>
+              </div>
         </div>
     );
 }
 
-export default DevicePage;
+export default HubPage;
