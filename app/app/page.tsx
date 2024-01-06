84d411bf800c75b5366ca581ede1390fa1556719
obanach <obanach@outlook.com>
2024-01-06 01:00:40 +0100

    app skeleton add

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,6 +3,7 @@ import CreateNewHub from "@/app/app/components/CreateNewHub";
 import React, {useEffect, useState} from "react";
 import {HubCard, HubCardEmpty, HubCardSkeleton} from "@/app/app/components/hub";
 import {HubType} from "@/app/app/type";
+import {Skeleton} from "@/components/ui/skeleton";
 
 const hubsData = [
     {
@@ -33,19 +34,18 @@ export default function App() {
         }, 1000)
     }, []);
 
+    if (loading) {
+        return <AppSkeleton/>
+    }
 
     return (
         <div className={''}>
-
-
             <div className={'mb-5 flex items-center'}>
                 <h2 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1] mr-auto">
                     List of hubs
                 </h2>
                 <CreateNewHub/>
             </div>
-
-
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
                 {
                     loading
@@ -62,4 +62,24 @@ export default function App() {
             </div>
         </div>
     );
+}
+
+const AppSkeleton: React.FC = () => {
+    return (
+        <div className={''}>
+            <div className={'mb-5 flex items-center'}>
+                <h2 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1] mr-auto">
+                    <Skeleton className="h-12 w-56"/>
+                </h2>
+                <Skeleton className="h-10 w-24"/>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                <HubCardSkeleton/>
+                <HubCardSkeleton/>
+                <HubCardSkeleton/>
+                <HubCardSkeleton/>
+                <HubCardSkeleton/>
+            </div>
+        </div>
+)
 }
